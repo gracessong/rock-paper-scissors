e4a07a6348ac4e70a9d44989aba114c184beee5a
gracessong <gracesongpt@gmail.com>
2022-02-11 22:15:25 -0700

    Rewrote JS with DOM and added CSS style to make the game interactive. README updated.

--- script.js
+++ script.js
@@ -18,20 +18,26 @@ function playRound(playerSelection) {
     playerSelection == paper && computerSelection == 'rock' ||
     playerSelection == scissors && computerSelection == 'paper') {
         playerScore += 1;
-        return 'player wins';
+        return 'You win!';
     } else if (playerSelection == rock && computerSelection == 'rock' ||
     playerSelection == paper && computerSelection == 'paper' ||
     playerSelection == scissors && computerSelection == 'scissors') {
-        return 'tie!';
+        return 'Tie! Keep playing.';
     } else {
         computerScore += 1;
-        return 'computer wins';
+        return 'Computer wins!';
     };
 };
 const container = document.querySelector('#container');
+
 const resultOutput = document.createElement('div');
+resultOutput.classList.add('result-output')
+
 const scoreOutputPlayer = document.createElement('div');
+scoreOutputPlayer.classList.add('score-output-player')
+
 const scoreOutputComp = document.createElement('div')
+scoreOutputComp.classList.add('score-output-computer')
 
 container.appendChild(resultOutput);
 container.appendChild(scoreOutputPlayer);
@@ -39,12 +45,12 @@ container.appendChild(scoreOutputComp);
 
 function endGame() {
     if (playerScore === 5) {
-        alert("GAME OVER, YOU BEAT THE COMPUTER!!");
+        document.querySelector('#final-result').textContent = 'YOU BEAT THE COMPUTER! THE MANKIND THANKS YOU!';
         rock.removeEventListener('click', playRock);
         paper.removeEventListener('click', playPaper);
         scissors.removeEventListener('click', playScissors);
     } else if (computerScore === 5) {
-        alert("GAME OVER, COMPUTER BEAT YOU!!");
+        document.querySelector('#final-result').textContent = 'COMPUTER BEAT YOU! THE MANKIND IS DOOMED THANKS TO YOU!';
         rock.removeEventListener('click', playRock);
         paper.removeEventListener('click', playPaper);
         scissors.removeEventListener('click', playScissors);
@@ -53,22 +59,22 @@ function endGame() {
 
 function playRock() {
     resultOutput.textContent = `${playRound(rock)}`
-    scoreOutputPlayer.textContent = `${playerScore}`
-    scoreOutputComp.textContent = `${computerScore}`
+    scoreOutputPlayer.textContent = `😎  ${playerScore}:`
+    scoreOutputComp.textContent = `${computerScore}  🖥`
     endGame();
 }
 
 function playPaper() {
     resultOutput.textContent = `${playRound(paper)}`
-    scoreOutputPlayer.textContent = `${playerScore}`
-    scoreOutputComp.textContent = `${computerScore}`
+    scoreOutputPlayer.textContent = `😎  ${playerScore}:`
+    scoreOutputComp.textContent = `${computerScore}  🖥`
     endGame();
 }
 
 function playScissors() {
     resultOutput.textContent = `${playRound(scissors)}`;
-    scoreOutputPlayer.textContent = `${playerScore}`;
-    scoreOutputComp.textContent = `${computerScore}`;
+    scoreOutputPlayer.textContent = `😎  ${playerScore}:`
+    scoreOutputComp.textContent = `${computerScore}  🖥`
     endGame()
 }
 
@@ -76,5 +82,25 @@ rock.addEventListener('click', playRock)
 paper.addEventListener('click', playPaper)
 scissors.addEventListener('click', playScissors)
 
+const header = document.querySelector('#header')
 
-    
+const headerDiv = document.createElement('div');
+headerDiv.textContent = 'The Great Rock Paper Scissors Game';
+headerDiv.style.cssText = 'font-size: 50px; color: white; font-family: roboto, tahoma, sans-serif; font-weight: 900';
+
+const subheadDiv = document.createElement('div');
+subheadDiv.setAttribute('style', 'white-space: pre;');
+subheadDiv.classList.add('subhead');
+subheadDiv.textContent = 'The AI has finally evolved to be self-aware and now has deemed the mankind to be unworthy of existing. \r\n You must now play against the computer to save mankind. \r\n Make your selection:'
+subheadDiv.style.cssText = 'font-size: 15px; color: white; font-family: roboto, tahoma, sans-serif; font-weight: 700;'
+
+header.appendChild(headerDiv)
+header.appendChild(subheadDiv)
+
+const scoreContainer = document.createElement('div');
+scoreContainer.classList.add('score-container');
+
+scoreContainer.appendChild(scoreOutputPlayer);
+scoreContainer.appendChild(scoreOutputComp)
+
+container.appendChild(scoreContainer)
